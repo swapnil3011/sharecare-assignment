07d5c51904ad804b6fd6705cbede40c5ab3cf124
swapnil3011 <swapnil.taparia@purplestylelabs.com>
2025-07-02 20:28:12 +0530

    add ignoring `dist` folder for `eslint` and `stylelint`

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,10 @@ import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
+  globalIgnores(['dist/**/*']),
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
